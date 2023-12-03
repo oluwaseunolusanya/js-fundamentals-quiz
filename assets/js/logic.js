bddecc889767d16ae793f1cf01b4393477521dab
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2023-12-03 18:36:38 +0000

    Added algorithms to scripts.

---
+++ assets/js/logic.js
@@ -0,0 +1,19 @@
+//1.  Add a 'click' eventlistener to the start button.
+//2.  Get number of questions from the local storage.
+//3.  Multiply the number by 30 seconds.
+//4.  Set timer to the result from step 3 and start counting down.
+//5.  Get the first question from local storage.
+//6.  Set the content of the page to the first question along with its choices.
+//7.  Add 'click' eventlisteners to the choices.
+//8.  Get the correct answer to the question from local storage.
+//9.  If the clicked choice is the same as the data from step 8:
+//     - Display 'Correct'.
+//     - Increment score by 10.
+//     - Move on to the next question.
+//10. If the clicked choice is not the same as the data from step 8:
+//     - Display 'Wrong'.
+//     - Decrement the timer by 10 seconds.
+//     - Move on to the next question
+//11. If timer is zero or all questions have been attempted.
+//    - End quiz
+//12. Display score and prompt user to submit their initials and score.
---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+//1. Create an object of questions
+let questions = {
+    question: 'Inside which HTML element do we put the JavaScript?',
+    choices: [
+        '<scripting>',
+        '<js>',
+        '<script>',
+        '<javascript>'
+    ],
+    answer: '<script>'
+}
+//2. Load questions to local storage when start button is clicked.
+let startButton = document.querySelector()
