4ae6a613da8399c6d80c05566ff564eeb2aa6a72
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2023-12-03 20:07:37 +0000

    Set timer limit in logic.js.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,21 @@
+//2.  Get number of questions from the local storage.
+let timelimit = parseInt(localStorage.getItem("totalQuestions"));
+
+//3.  Multiply the number by 30 seconds.
+timelimit *= 30;
+
+//4.  Set timer to the result from step 3 and start counting down.
+let timer = document.querySelector("#time");
+
 //1.  Add a 'click' eventlistener to the start button.
-let timelimit = 0;
 let questionsFromStorage;
 startQuiz.addEventListener('click', function(event) {
     event.preventDefault(); 
-    questionsFromStorage = JSON.parse(localStorage.getItem("questions"));
-    console.log(questionsFromStorage);
+    timer.textContent = timelimit;
+
 
 })
-//2.  Get number of questions from the local storage.
-//3.  Multiply the number by 30 seconds.
-//4.  Set timer to the result from step 3 and start counting down.
+
 //5.  Get the first question from local storage.
 //6.  Set the content of the page to the first question along with its choices.
 //7.  Add 'click' eventlisteners to the choices.
