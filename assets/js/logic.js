e59dc9743ed673ef728f69c977938018a541b5ef
Oluwaseun Olusanya <oluwaseun_olusanya@yahoo.com>
2023-12-03 21:01:45 +0000

    Hide start screen.

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,27 @@
-//2.  Get number of questions from the local storage.
+//Get number of questions from the local storage and multiply by 30 seconds.
 let timelimit = parseInt(localStorage.getItem("totalQuestions"));
-
-//3.  Multiply the number by 30 seconds.
 timelimit *= 30;
 
-//4.  Set timer to the result from step 3 and start counting down.
+//Set timer to the result from step 3 and start counting down.
 let timer = document.querySelector("#time");
 
-//1.  Add a 'click' eventlistener to the start button.
-let questionsFromStorage;
+//Add a 'click' eventlistener to the start button.
+let questionsFromStorage = JSON.parse(localStorage.getItem("questions"));
+let firstQuestionKey = Object.keys(questionsFromStorage)[0];
+let firstQuestion = questionsFromStorage.firstQuestionKey;
+console.log(firstQuestion);
+
+//Hide the start screen and load the first question from memory.
+let startScreen = document.querySelector("#start-screen");
+let questionsSection = document.querySelector("#questions");
+let questionsSectionHeading = document.querySelector("#question-title");
+
 startQuiz.addEventListener('click', function(event) {
     event.preventDefault(); 
     timer.textContent = timelimit;
-
-
+    startScreen.setAttribute("class", "hide");
+    questionsSection.setAttribute("class", "start")
+    questionsSectionHeading.textContent = firstQuestion;
 })
 
 //5.  Get the first question from local storage.
